f6e562c0f5c856767dae98ef92bf64b9b5d5a3fe
Cedric Kisema <cedric.kisema@gmail.com>
2016-06-25 15:40:38 +0100

    front page redesign

---
+++ js/main.js
@@ -0,0 +1,16 @@
+---
+layout: null
+---
+$$('.trigger').addEvent('click', function(event) {
+  var intro = $$('.intro')[0];
+  var trigger = $$('.trigger')[0];
+
+  if ('500px' > intro.getStyle('width')) {
+    intro.tween('width', '500px');
+  }
+
+  var body = $$('body')[0];
+  body.setStyle('overflow-y', "auto");
+
+  event.preventDefault();
+});
